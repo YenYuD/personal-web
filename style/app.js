5f25d4142a4662e703870a029ad63d2096b7f47a
yenyu <diaoemily0830@gmail.com>
2022-03-23 17:07:38 +0800

    0322_1

---
+++ style/app.js
@@ -0,0 +1,7 @@
+const text = document.querySelectorAll(".thePaths");
+
+console.log(text[0].getTotalLength());
+
+for (let i = 0; i < text.length; i++) {
+  console.log(`text number ${i} length is ${text[i].getTotalLength()}`);
+}
