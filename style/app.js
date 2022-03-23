da4c5f64c6290b1e988c28f116affab9ab8b9cc5
yenyu <diaoemily0830@gmail.com>
2022-03-23 18:41:30 +0800

    0322_2

--- style/app.js
+++ style/app.js
@@ -5,3 +5,12 @@ console.log(text[0].getTotalLength());
 for (let i = 0; i < text.length; i++) {
   console.log(`text number ${i} length is ${text[i].getTotalLength()}`);
 }
+
+const lastWord = document.querySelector("#seventeenth");
+console.log(lastWord);
+
+const animation = document.querySelector("div.animation");
+lastWord.addEventListener("animationend", () => {
+  animation.style =
+    "transition: all 1s ease; opacity: 0; pointer-events: none;";
+});
