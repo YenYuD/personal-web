4be21cb655cacce010cc602f63707d323a870627
yenyu <diaoemily0830@gmail.com>
2022-03-25 16:59:57 +0800

    0325

--- style/app.js
+++ style/app.js
@@ -14,3 +14,19 @@ lastWord.addEventListener("animationend", () => {
   animation.style =
     "transition: all 1s ease; opacity: 0; pointer-events: none;";
 });
+
+// const svgGroup = document.querySelectorAll(".logo > svg > path");
+// svgGroup.forEach((item) => {
+//   const totalLength = item.getTotalLength();
+//   item.style = `
+//         stroke-dasharray: ${Math.ceil(totalLength)}px;
+//         stroke-dashoffset: ${Math.ceil(totalLength)}px;
+//     `;
+//   setTimeout(function () {
+//     item.style = `
+//     stroke-dasharray: ${Math.ceil(totalLength)}px;
+//     stroke-dashoffset: 0px;
+//     transition: all 0.6s ease-in;
+//     `;
+//   }, 3800);
+// });
